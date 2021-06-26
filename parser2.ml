3f5338b9ea160e1e0ca6a910bb2d0a15853b9255
Elliot Spargo <nspargo@andrew.cmu.edu>
2021-06-26 14:14:19 -0400

    lambda and ret done

--- parser2.ml
+++ parser2.ml
@@ -1,9 +1,13 @@
 let rec parse_term toks =
-  (circ term (*look for body of the lambda *)
+  ((circ term (*look for body of the lambda *)
     ((keycircr (keycircl (circ (repeat id) id) (*look for all the captured vars*)
     "\\") (*looking for a lambda*)
-        ".") >> cons)) >> makeLambda
-|:|
+        ".") >> cons)) >> makeLambda)
+  |:| ((circ (repeat atom) atom) >> applyList) (*single atom or application of atoms*)
+  |:| (keycircl atom
+         "ret") (*looking for a ret*)
+  |:| (keycircl
+      "bind") (*looking for a bind*)
 
 and let rec parse_atom toks =  (id >> free)
                                |:| ((keycircr ")" (keycircl term "(")) >> pi1)
