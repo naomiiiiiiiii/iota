e9f229083dd177d2d9b2748275d28bd53314bef5
Elliot Spargo <nspargo@andrew.cmu.edu>
2021-06-28 21:01:42 -0400

    have to rename parser files not to conflict with core

--- source_parsing.ml
+++ source_parsing.ml
@@ -10,8 +10,10 @@ let o = Fn.compose
 
 (*shouldnt ever need to parse types,just print them from AST, going other way*)
 
+let () = Printf.printf "hurg \n"
+
 module type PARSE_TERM = sig
-  let val read : string -> Source.exp end
+  val read : string -> Source.exp end
 
 module SourceKey : Scanner.KEYWORD = struct
   let alpha_num = ["ret"; "bind"; "let"; "ref"; "in"]
@@ -25,18 +27,19 @@ module SourceLex : Scanner.LEXICAL = Scanner.Lexical(SourceKey)
 module SourceParsing : Parser.PARSE = Parser.Parsing(SourceLex)
 
 
-module ParseTerm : PARSE_TERM = let rec parse_term toks = struct
-                                  open Parser
-
-  ((circ term (*look for body of the lambda *)
+module ParseTerm = struct
+  open Parser
+    let test = Parser.epsilon end
+ (*let rec parse_term toks =                                 
+  (* *)((circ term (*look for body of the lambda *)
       ((keycircr "."
           (keycircl
              (circ (repeat id) id) (*look for all the captured vars*)
              "\\") (*looking for a lambda*)
        ) >> cons) (*collects identifiers into a list*)
    ) >> Source.absList) (*turns list of identifiers and body into a lam*)
-  |:| ((circ (repeat atom) atom) >> Source.applyList) (*single atom or application of atoms*)
-  |:| ((keycircl atom "ret") >> Source.Ret) (*looking for a ret. make ret: exp -> exp*)
+       |:| ((circ (repeat atom) atom) >> Source.applyList) **) (*single atom or application of atoms*)
+ (* |:| ((keycircl atom "ret") >> Source.Ret) (*looking for a ret. make ret: exp -> exp*)
   |:| ((keycircl (keycircl (circ (keycircr ")" term) (*2nd term*)
                              (keycircr "," term)) (*1st term*)
                "(" )
@@ -48,9 +51,7 @@ module ParseTerm : PARSE_TERM = let rec parse_term toks = struct
                                          makeref: ((string x exp) x exp) -> exp *)
   |:| ((circ term (keycircr ":=" id)) >> Source.Assign) (*: (string * exp) -> exp*)
   |:| ((keycircl id "!") >> Source.Deref) (*: string -> exp*)
-  |:| (startp >> Source.Star) (*: unit -> exp*)
-
-
-and let rec parse_atom toks =  (id >> free)
+  (|:|) (startp >> Source.Star) (*: unit -> exp*)
+and parse_atom toks =  (id >> free)
                                |:| ((keycircr ")" (keycircl term "(")) >> pi1)
-                               |:| (natp >> nat)
+                               |:| (natp >> nat)*)
