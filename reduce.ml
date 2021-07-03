b0ae9524f79057d05a72ca64a1572e54df4764b0
Elliot Spargo <nspargo@andrew.cmu.edu>
2021-07-03 16:54:51 -0400

    reduce 1st draft compiling

--- reduce.ml
+++ reduce.ml
@@ -1,29 +1,27 @@
+open Base
 open Core_kernel
 open Source
 
 module type STATE = sig
-  type store_type = (int, exp, Int.comparator_witness) Map.t
-  type env_type = (string, exp, String.comparator_witness) Map.t
-  val store : store_typ 
-  val env : env_typ
+  type store_type = (int, exp, Int.comparator_witness) Map.t (*for storing references*)
+  type env_type = (string, exp, String.comparator_witness) Map.t (*for storing identifiers declared earlier in the file being evaluated*)
+  val store : store_type 
+  val env : env_type
 end
 
 module type REDUCER = sig 
-exception RuntimeError of string
-type store_type
-val eval: exp * store_type -> exp * store_type
-
+  exception RuntimeError of string
+  module State : STATE
+  val eval: exp -> exp * State.store_type
 end
 
 (*probably need to put a with here*)
 module Reducer (State: STATE): REDUCER = struct
 
   exception RuntimeError of string
-
-  type env_type = State.env_type
-  type store_typ = State.store_type
-
-  val env = State.env
+  module State = State
+  let env = State.env
+  let store = State.store
 
   let get_loc loc = match loc with
       Loc n -> n
@@ -32,52 +30,47 @@ module Reducer (State: STATE): REDUCER = struct
 (*adds a new reference containing m to s, returns the index of this new reference
 and the new store *)
 let add_ref m s = let index = (Map.length s) in match (Map.add s ~key:index ~data:m) with
-    Ok snew -> (Loc index, snew)
-  | Duplicate -> raise RuntimeError "tried to overwite location " ^ (Int.to_string index)
+   `Duplicate -> raise (RuntimeError ("tried to overwite location " ^ (Int.to_string index)))
+  |`Ok snew -> (Loc index, snew)
 
-let assign_ref loc m s = let index = (get_loc loc)
-  and change_fn = (fun v -> match v with
-        None -> raise RuntimeError ("assigned to uninitialized location " ^ (Int.to_string index))
-      | Some m_old -> Some m
+let assign_ref loc m s = let index = (get_loc loc) in
+  let change_fn = (fun v -> match v with
+        None -> raise (RuntimeError ("assigned to uninitialized location " ^ (Int.to_string index)))
+      | Some _ -> Some m
      ) in
-  (Star, (Map.change s index change_fn))
+  (Star, (Map.change s index ~f:change_fn))
 
 let deref loc s = let index = (get_loc loc) in match (Map.find s index) with
     None -> raise (RuntimeError ("dereferencing uninitialized location " ^ (Int.to_string index)))
   | Some m -> (m, s)
 
-let rec eval (m, s) = match m with
-    Free id -> eval ((Map.find_exn env id), s)
-  | Star -> (m, s) (*combine all these caes into one case if this compiles*)
-  | Nat _ -> (m, s)
-  | Loc _ -> (m, s)
-  | Lam _ -> (m, s)
-  | Ap(fn, arg) -> let (fnval, s1) = (eval (fn, s)) in
-    let (argval, s2) = (eval (arg, s1)) in
+let rec eval_help (m, s) = match m with
+    Free id -> eval_help ((Map.find_exn env id), s)
+  | Star | Nat | Loc _ | Lam _ | Ret _ | Ref _ | Asgn _ | Deref _ -> (m, s) (*ret, ref, asgn, deref are suspended computations*)
+  | Ap(fn, arg) -> let (fnval, s1) = (eval_help (fn, s)) in
+    let (argval, s2) = (eval_help (arg, s1)) in
     (match fnval with
-      Lam(_, body) -> eval ((subst 0 argval body), s2)
+      Lam(_, body) -> eval_help ((subst 0 argval body), s2)
      | _  -> (Ap(fnval, argval), s2))
-  | Ret(m0) -> (m, s) (*suspended! *)
-  | Bind(m1, m2) -> let (m1cval, s1) = (eval (m1, s)) in (*e1cval should be ret, ref, asn, deref*)
+  | Ret _ -> (m, s) (*suspended! *)
+  | Bind(m1, m2) -> let (m1cval, s1) = (eval_help (m1, s)) in (*e1cval should be ret, ref, asn, deref*)
     let (m1val, s2) = bind_eval(m1cval, s1) in (*releases the computation in e1cval*)
-    eval (subst 0 m1val m2, s2)
-  | Ref(m0) -> (m, s)
-  | Asgn(m0) -> (m, s)
-  | Deref(m0) -> (m, s)
-  | Bound _
-  | exception RuntimeError _ -> m
-  | exception _ -> raise (RuntimeError ("failing on " ^ (exp_to_string m)))
+    eval_help (subst 0 m1val m2, s2)
+  | exception RuntimeError err -> raise (RuntimeError err)
+  | Bound _ | exception _ -> raise (RuntimeError ("failing on " ^ (Display.exp_to_string m)))
 
 (*evaluates what is underneath the structure that suspends m and carries out any effects
 gives (m', s') where m' is the finished result of the once-delayed computation
 and s' is the new store*)    
-and bind_eval(m, s) = match m with
-    Ret(m0) -> eval (m0, s)
-  | Ref(m0) -> let (mval, s1) = eval(m0, s) in (add_ref mval s1)
-  | Assn(loc, m1) -> let (locval, s1) = eval(loc, s) in
-    let (m1val, s2) = eval(m1, s1) in (assign_ref locval m1val s2) 
-  | Deref(loc) -> let (locval, s1) = eval(loc, s) in (deref locval s1)
-  | _ -> raise (RuntimeError ((exp_to_string m) ^ " not bindable"))
+and bind_eval (m, s) = match m with
+    Ret(m0) -> eval_help (m0, s)
+  | Ref(m0) -> let (mval, s1) = eval_help(m0, s) in (add_ref mval s1)
+  | Asgn(loc, m1) -> let (locval, s1) = eval_help(loc, s) in
+    let (m1val, s2) = eval_help(m1, s1) in (assign_ref locval m1val s2) 
+  | Deref(loc) -> let (locval, s1) = eval_help (loc, s) in (deref locval s1)
+  | _ -> raise (RuntimeError ((Display.exp_to_string m) ^ " not bindable"))
 
+
+let eval m = eval_help (m, store)
 
 end
--- reduce.mli
+++ reduce.mli
@@ -5,16 +5,14 @@ open Source
 module type STATE = sig
   type store_type = (int, exp, Int.comparator_witness) Map.t (*for storing references*)
   type env_type = (string, exp, String.comparator_witness) Map.t (*for storing identifiers declared earlier in the file being evaluated*)
-  val store : store_typ 
-  val env : env_typ
+  val store : store_type 
+  val env : env_type
 end
 
 module type REDUCER = sig 
-exception RuntimeError of string
-type store_type
-type env_type
-val env: env_type
-val eval: exp * store_type -> exp * store_type
+  exception RuntimeError of string
+  module State : STATE
+  val eval: exp -> exp * State.store_type
 
 end
 
