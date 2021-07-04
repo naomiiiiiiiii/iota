aeee032f9a2d43d22828550e998ee8d1dbdcfca0
Elliot Spargo <nspargo@andrew.cmu.edu>
2021-07-03 22:33:59 -0400

    wrote display test cases

--- test.ml
+++ test.ml
@@ -11,11 +11,9 @@ include Checker
 module MyString = String_lib
 
 open Display
-
 open Source
 
 let empty = (Map.empty (module String))
-let testenv = Map.set empty ~key:"f" ~data:(Arr(Nattp, Reftp(Unit)))
 
 module State: TYPESTATE = struct
   let env = testenv 
@@ -32,6 +30,17 @@ let test = read "bind(ret(5), \\(x: Nat).ret ())"
 let test1 = read "bind(ret(ref(5)), \\(x: Ref(Nat)).ret(!x))"
 let test1 = read "f 5" with f in env
     *)
+
+(*final tests for display*)
+
+let test1 = "let shouldbe5 = bind(ret(5), \n.bind(ref(0), \r.bind(r:=n, !r)))" (*r should come out 5*)
+
+let test2 = "let theanswer = bind(ref(40), \r1.bind(ref(!r1 + 1), \r2. bind(r2 := !r2 + 1, !r2)))" (*r2 should come out 42*)
+
+let test3= "let plus_ref = ref (\(n1: Nat)\(n2: Nat). n1 + n2
+let plus1 = \(n2: Nat).!plus_ref 1 n2
+let shouldbetwo = plus1 1 
+"
 
 
 let test1 = read "f 5"
